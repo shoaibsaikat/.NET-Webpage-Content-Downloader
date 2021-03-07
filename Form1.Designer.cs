1fc606276e3e366e7f1670884f008f94f0a26868
Mina Shoaib Rahman <shoaibsaikat@gmail.com>
2021-03-07 17:05:35 +0600

    Added files, todo: 1. add threading support, 2. add html formating, 3. add copy button

---
+++ Form1.Designer.cs
@@ -0,0 +1,160 @@
+﻿
+namespace Web_Page_Content_Downloader
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBoxUrl = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.numericUpDownCount = new System.Windows.Forms.NumericUpDown();
+            this.buttonDone = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBoxTag = new System.Windows.Forms.TextBox();
+            this.richTextBoxOutput = new System.Windows.Forms.RichTextBox();
+            this.labelAuthor = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownCount)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "URL";
+            // 
+            // textBoxUrl
+            // 
+            this.textBoxUrl.Location = new System.Drawing.Point(16, 30);
+            this.textBoxUrl.Name = "textBoxUrl";
+            this.textBoxUrl.Size = new System.Drawing.Size(772, 20);
+            this.textBoxUrl.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 53);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(63, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Page Count";
+            // 
+            // numericUpDownCount
+            // 
+            this.numericUpDownCount.Location = new System.Drawing.Point(16, 69);
+            this.numericUpDownCount.Maximum = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            0});
+            this.numericUpDownCount.Name = "numericUpDownCount";
+            this.numericUpDownCount.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDownCount.TabIndex = 4;
+            // 
+            // buttonDone
+            // 
+            this.buttonDone.Location = new System.Drawing.Point(19, 139);
+            this.buttonDone.Name = "buttonDone";
+            this.buttonDone.Size = new System.Drawing.Size(75, 23);
+            this.buttonDone.TabIndex = 5;
+            this.buttonDone.Text = "Done";
+            this.buttonDone.UseVisualStyleBackColor = true;
+            this.buttonDone.Click += new System.EventHandler(this.buttonDone_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(16, 96);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(90, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "HTML Tag Name";
+            // 
+            // textBoxTag
+            // 
+            this.textBoxTag.Location = new System.Drawing.Point(19, 113);
+            this.textBoxTag.Name = "textBoxTag";
+            this.textBoxTag.Size = new System.Drawing.Size(100, 20);
+            this.textBoxTag.TabIndex = 7;
+            // 
+            // richTextBoxOutput
+            // 
+            this.richTextBoxOutput.Location = new System.Drawing.Point(19, 169);
+            this.richTextBoxOutput.Name = "richTextBoxOutput";
+            this.richTextBoxOutput.Size = new System.Drawing.Size(769, 269);
+            this.richTextBoxOutput.TabIndex = 8;
+            this.richTextBoxOutput.Text = "";
+            // 
+            // labelAuthor
+            // 
+            this.labelAuthor.AutoSize = true;
+            this.labelAuthor.Location = new System.Drawing.Point(586, 113);
+            this.labelAuthor.Name = "labelAuthor";
+            this.labelAuthor.Size = new System.Drawing.Size(33, 13);
+            this.labelAuthor.TabIndex = 9;
+            this.labelAuthor.Text = "None";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.labelAuthor);
+            this.Controls.Add(this.richTextBoxOutput);
+            this.Controls.Add(this.textBoxTag);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.buttonDone);
+            this.Controls.Add(this.numericUpDownCount);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBoxUrl);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.Text = "Web Content Downloader";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownCount)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBoxUrl;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.NumericUpDown numericUpDownCount;
+        private System.Windows.Forms.Button buttonDone;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBoxTag;
+        private System.Windows.Forms.RichTextBox richTextBoxOutput;
+        private System.Windows.Forms.Label labelAuthor;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.IO;
+using HtmlAgilityPack;
+using HtmlDocument = HtmlAgilityPack.HtmlDocument;
+
+namespace Web_Page_Content_Downloader
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            labelAuthor.MaximumSize = new Size(300, 300);
+            labelAuthor.Text = "Developed by: Mina Shoaib Rahman\nMail: shoaibsaikat@gmail.com";
+        }
+
+        private int lastPageIndex(String url)
+        {
+            int i = url.Length - 1;
+            for (; i >= 0; i--)
+            {
+                if (url[i] < '0' || url[i] > '9')
+                    break;
+            }
+            return i;
+        }
+
+        private void buttonDone_Click(object sender, EventArgs e)
+        {
+            // http://mis.molwa.gov.bd/freedom-fighter-list?division_id=3&district_id=21&thana_id=192&page=189
+            var url = textBoxUrl.Text;
+            var tag = textBoxTag.Text;
+
+            var lastIndex = lastPageIndex(url);
+            var baseUrl = url.Remove(lastIndex + 1);
+
+            if (tag != null)
+            {
+                using (WebClient client = new WebClient()) // WebClient class inherits IDisposable
+                {
+                    var page = 1;
+                    while (page <= numericUpDownCount.Value)
+                    {
+                        Stream data = client.OpenRead(baseUrl + page);
+                        StreamReader reader = new StreamReader(data);
+                        string html = reader.ReadToEnd();
+                        var htmlDoc = new HtmlDocument();
+                        htmlDoc.LoadHtml(html);
+
+                        var htmlData = htmlDoc.DocumentNode.SelectNodes("//" + tag).ToList();
+                        for (int i = 1; i < htmlData.Count; i++) // skip first row, as it's a header
+                        {
+                            richTextBoxOutput.Text += htmlData[i].OuterHtml;
+                        }
+                        page++;
+                    }
+                }
+            }
+        }
+    }
+}
